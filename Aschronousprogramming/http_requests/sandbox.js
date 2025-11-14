59a185e6991c9bf9731769707f097eae9441d61d
Emelyne1024 <emelyneishimwe749@gmail.com>
2025-11-14 14:46:33 +0200

    Update: Making the codes reusable

--- Aschronousprogramming/http_requests/sandbox.js
+++ Aschronousprogramming/http_requests/sandbox.js
@@ -1,16 +1,24 @@
-const request = new XMLHttpRequest()
+const getTodos = ((callback)=>{
 
-request.addEventListener('readystatechange', ()=>{
-    // console.log(request, request.readystate) There are total of 4 states
-    if(request.readyState === 4 && request.status === 400){ //If done as the 4th state is done and its contains the data
-        console.log(request.responseText)// Display the JSON data to the console
-    }
-    else{
-        
-        console.log("fetching error, data not found")
+    const request = new XMLHttpRequest()
 
-    }
+    request.addEventListener('readystatechange', ()=>{
+        // console.log(request, request.readystate) There are total of 4 states
+        if(request.readyState === 4 && request.status=== 200 ){ //If done as the 4th state is done and its contains the data
+            callback(undefined, request.responseText)// Display the JSON data to the console
+        }
+        else{
+            callback("Fetch not found", undefined)
+        }
+    })
+
+    request.open('GET', 'https://jsonplaceholder.typicode.com/todos/')
+    request.send()
+
+
 })
 
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos/')
-request.send()
+getTodos((error, data)=>{
+    console.log("Callback fired")
+    console.log(data, error)
+})
