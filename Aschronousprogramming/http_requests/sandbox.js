3fcf1f5ee0d1becfb7861bb65d5363672ed734f1
Emelyne1024 <emelyneishimwe749@gmail.com>
2025-11-17 18:56:15 +0200

    Fetching data and using the JSON data

--- Aschronousprogramming/http_requests/sandbox.js
+++ Aschronousprogramming/http_requests/sandbox.js
@@ -5,9 +5,10 @@ const getTodos = ((callback)=>{
     request.addEventListener('readystatechange', ()=>{
         // console.log(request, request.readystate) There are total of 4 states
         if(request.readyState === 4 && request.status=== 200 ){ //If done as the 4th state is done and its contains the data
-            callback(undefined, request.responseText)// Display the JSON data to the console
+            const data = JSON.parse(request.responseText)
+            callback(undefined, data)// Display the JSON data to the console
         }
-        else{
+        else if(request.readyState === 4){
             callback("Fetch not found", undefined)
         }
     })
@@ -18,7 +19,19 @@ const getTodos = ((callback)=>{
 
 })
 
+console.log(1)
+console.log(2)
+
 getTodos((error, data)=>{
     console.log("Callback fired")
-    console.log(data, error)
+    if(error){
+        console.log("Couldn't fetch data")
+    }
+    else{
+        console.log(data)
+    }
 })
+
+
+console.log(3)
+console.log(4)
