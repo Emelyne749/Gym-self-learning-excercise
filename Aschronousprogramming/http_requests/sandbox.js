2a7fa3c4516bb86cb6781c31f8ac0f06be10f473
Emelyne1024 <emelyneishimwe749@gmail.com>
2025-11-19 14:42:11 +0200

    Lesson on callback hell

--- Aschronousprogramming/http_requests/sandbox.js
+++ Aschronousprogramming/http_requests/sandbox.js
@@ -5,8 +5,8 @@ const getTodos = ((callback)=>{
     request.addEventListener('readystatechange', ()=>{
         // console.log(request, request.readystate) There are total of 4 states
         if(request.readyState === 4 && request.status=== 200 ){ //If done as the 4th state is done and its contains the data
-            const data = JSON.parse(request.responseText)
-            callback(undefined, data)// Display the JSON data to the console
+            const datas = JSON.parse(request.responseText)
+            callback(undefined, datas)// Display the JSON data to the console
         }
         else if(request.readyState === 4){
             callback("Fetch not found", undefined)
