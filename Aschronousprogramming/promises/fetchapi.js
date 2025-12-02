f4496e072af782573c14cf176a0e3849ce1fca3f
Emelyne1024 <emelyneishimwe749@gmail.com>
2025-12-02 18:19:27 +0200

    Async and a wait used in promise chaining

--- Aschronousprogramming/promises/fetchapi.js
+++ Aschronousprogramming/promises/fetchapi.js
@@ -1,6 +1,17 @@
-fetch('elliot.json').then((response)=>{
-    console.log('resolved', response)
-    return response.json()//This will return a promise
-}).then(data=>console.log(data)).catch((err)=>{
-    console.log('rejected', err)
-})
+const getTodos = async () =>{
+    const response = await fetch('elliot.json')
+    const data = await response.json()
+    console.log('resolved', data)
+
+}
+
+getTodos()
+
+
+
+// fetch('elliot.json').then((response)=>{
+//     console.log('resolved', response)
+//     return response.json()//This will return a promise
+// }).then(data=>console.log(data)).catch((err)=>{
+//     console.log('rejected', err)
+// })
