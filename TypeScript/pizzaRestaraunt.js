fc33f47075eb7e121ae8802d056d80ca55c4da0e
Emelyne1024 <emelyneishimwe749@gmail.com>
2026-01-06 18:58:04 +0200

    Pasting the codes from the caurse videos to test type script error detection efficiency

--- TypeScript/pizzaRestaraunt.js
+++ TypeScript/pizzaRestaraunt.js
@@ -1,42 +1,84 @@
+// MY CODES
+// const menu = [
+//   { name: "Margherita", price: 8},
+//   { name: "Pepperoni", price: 10},
+//   { name: "Hawaiian", price: 10},
+//   { name: "Veggie", price: 9},
+// ]
+
+
+// let cashInRegister = 100
+// let orderQue = []
+// let nextOrderId = 0
+
+// function AddNewPizza(pizza){
+//     menu.push(pizza)
+// } 
+
+
+// function placeOrder(pname){
+//     let pizza
+//     menu.forEach(pizzaObj => {
+//         for(value of Object.values(pizzaObj)){
+//             if(value == pname){
+//                 pizza = pizzaObj
+//                 break
+//             }
+//         }
+//     })
+
+//     cashInRegister = cashInRegister + pizza.price
+//     orderObj = {pizza:pizza , status : 'ordered', id:nextOrderId}
+//     orderQue.push(orderObj)
+//     nextOrderId++
+//     return orderObj
+// }
+
+
+// function completeOrder(orderID){
+//     let completedOrder = orderQue.filter(order => order.id = orderID)
+//     completedOrder.status = "completed"
+
+//     return completedOrder
+// }
+
+
 const menu = [
-  { name: "Margherita", price: 8},
-  { name: "Pepperoni", price: 10},
-  { name: "Hawaiian", price: 10},
-  { name: "Veggie", price: 9},
+    { name: "Margherita", price: 8 },
+    { name: "Pepperoni", price: 10 },
+    { name: "Hawaiian", price: 10 },
+    { name: "Veggie", price: 9 },
 ]
 
+const cashInRegister = 100
+const nextOrderId = 1
+const orderQueue = []
 
-let cashInRegister = 100
-let orderQue = []
-let nextOrderId = 0
-
-function AddNewPizza(pizza){
-    menu.push(pizza)
-} 
-
-
-function placeOrder(pname){
-    let pizza
-    menu.forEach(pizzaObj => {
-        for(value of Object.values(pizzaObj)){
-            if(value == pname){
-                pizza = pizzaObj
-                break
-            }
-        }
-    })
-
-    cashInRegister = cashInRegister + pizza.price
-    orderObj = {pizza:pizza , status : 'ordered', id:nextOrderId}
-    orderQue.push(orderObj)
-    nextOrderId++
-    return orderObj
+function addNewPizza(pizzaObj) {
+    menu.push(pizzaObj)
 }
 
-
-function completeOrder(orderID){
-    let completedOrder = orderQue.filter(order => order.id = orderID)
-    completedOrder.status = "completed"
-
-    return completedOrder
+function placeOrder(pizzaName) {
+    const selectedPizza = menu.find(pizzaObj => pizzaObj.name === pizzaName)
+    cashInRegister += selectedPizza.price
+    const newOrder = { id: nextOrderId++, pizza: selectedPizza, status: "ordered" }
+    orderQueue.push(newOrder)
+    return newOrder
 }
+ 
+function completeOrder(orderId) {
+    const order = orderQueue.find(order => order.id === orderId)
+    order.status = "completed"
+    return order
+}
+
+addNewPizza({ name: "Chicken Bacon Ranch", cost: 12 })
+addNewPizza({ name: "BBQ Chicken", cost: 12 })
+addNewPizza({ name: "Spicy Sausage", cost: 11 })
+
+placeOrder("Chicken Bacon Ranch")
+completeOrder("1")
+
+console.log("Menu:", menu)
+console.log("Cash in register:", cashInRegister)
+console.log("Order queue:", orderQueue)
